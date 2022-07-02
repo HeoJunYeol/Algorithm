5915a5e4e95b27554d27e934aa7f58e56dcf4ec4
HeoJunYeol <wnsduf9160@naver.com>
2022-07-03 00:00:02 +0900

    2022-07-02

--- src/Main_2908.java
+++ src/Main_2908.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 public class Main_2908 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        StringBuffer revereda = new StringBuffer(a);
+        a = revereda.reverse().toString();
+        StringBuffer reveredb = new StringBuffer(b);
+        b = reveredb.reverse().toString();
+        int answer = Math.max(Integer.parseInt(a),Integer.parseInt(b));
+        System.out.println(answer);
 
     }
 }
