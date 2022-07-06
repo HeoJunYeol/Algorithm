a996abb89ccc7d26119121414daaf46082aab440
HeoJunYeol <wnsduf9160@naver.com>
2022-07-06 10:10:37 +0900

    2022-07-06

--- src/Main_3052.java
+++ src/Main_3052.java
@@ -5,13 +5,8 @@ public class Main_3052 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         HashSet<Integer> set = new HashSet<>();
-        int data;
 
-        for(int i=0; i<10; i++){
-            data = sc.nextInt();
-            data %= 42;
-            set.add(data);
-        }
+        for(int i=0; i<10; i++) set.add(sc.nextInt()%42);
         System.out.println(set.size());
 
     }
