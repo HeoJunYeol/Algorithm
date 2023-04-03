f7f150fc655699cf9eae4fe521cbc9c87d560826
junyeol <106057412+HeoJunYeol@users.noreply.github.com>
2023-04-03 16:09:24 +0900

    [unrated] Title: 추억 점수, Time: 0.02 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -2,15 +2,13 @@ class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
         for (int i=0; i<photo.length;i++){
-            int cnt = 0;
             for(int j=0; j<photo[i].length; j++){
                 for (int k=0; k<name.length; k++){
                     if(photo[i][j].equals(name[k])){
-                        cnt += yearning[k];
+                        answer[i] += yearning[k];
                     }
                 }
             }
-            answer[i] = cnt;
         }
         return answer;
     }
