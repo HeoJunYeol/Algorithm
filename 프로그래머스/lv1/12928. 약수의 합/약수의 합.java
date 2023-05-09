cab0d6ef7f469393cefcab0a22891a63146f5cf1
junyeol <106057412+HeoJunYeol@users.noreply.github.com>
2023-05-09 23:27:52 +0900

    [level 1] Title: 약수의 합, Time: 1.10 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -1,14 +1,16 @@
+import java.util.HashSet;
+
 public class Solution {
     public int solution(int n) {
-        int answer =0;
-        for (int i=1; i<=Math.sqrt(n); i++){
-            if(n % i ==0 ){
-                answer += i;
-                if( n / i != i){
-                    answer += n / i;
+    HashSet<Integer> set = new HashSet<>();
+        for (int i =1; i<=Math.sqrt(n); i++){
+            if(n % i ==0){
+                set.add(i);
+                if(n % i != i){
+                    set.add(n/i);
                 }
             }
         }
-        return answer;
+        return set.stream().mapToInt(e -> e).sum();
     }
 }
