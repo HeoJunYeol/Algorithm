195bb97c6316c35f7d72cb09f95c242179c3ec8f
junyeol <106057412+HeoJunYeol@users.noreply.github.com>
2023-05-19 17:38:12 +0900

    [level 1] Title: 폰켓몬, Time: 2.16 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,15 +1,14 @@
 import java.util.HashSet;
-import java.util.Set;
 
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> s = new HashSet<>();
+        HashSet<Integer> s = new HashSet<>();
         for (int i:nums){
             s.add(i);
         }
         if(nums.length / 2 > s.size()){
             return s.size();
         }
-        else return nums.length/2;
+        return nums.length/2;
     }
 }
