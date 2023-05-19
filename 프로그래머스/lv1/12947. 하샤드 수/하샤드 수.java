93c1443935bc4565c3c8a861572cf6fbb1100dbf
junyeol <106057412+HeoJunYeol@users.noreply.github.com>
2023-05-19 16:22:01 +0900

    [level 1] Title: 하샤드 수, Time: 0.17 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean solution(int x) {
-        String str = Integer.toString(x);
-        String[] s = str.split("");
+        String[] s = String.valueOf(x).split("");
         int answer =0;
         for(int i=0; i<s.length;i++){
             answer += Integer.parseInt(s[i]);
