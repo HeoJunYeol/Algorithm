9264b717341319ceef79d216dc16a817013c23da
junyeol <106057412+HeoJunYeol@users.noreply.github.com>
2023-08-10 23:35:12 +0900

    [Silver IV] Title: 큐, Time: 236 ms, Memory: 19256 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -5,12 +5,14 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+
 public class Main {
     public static void main(String[]args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         Queue<Integer> queue = new LinkedList<>();
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
         while (n --> 0){
             st = new StringTokenizer(br.readLine());
             String str = st.nextToken();
@@ -18,40 +20,42 @@ public class Main {
                 queue.add(Integer.parseInt(st.nextToken()));
             } else if (str.equals("pop")) {
                 if (queue.isEmpty()){
-                    System.out.println(-1);
+                    sb.append(-1).append('\n');
                 }
                 else {
-                    System.out.println(queue.poll());
+                    sb.append(queue.poll()).append('\n');
                 }
             } else if (str.equals("size")) {
-                System.out.println(queue.size());
+                sb.append(queue.size()).append('\n');
             } else if (str.equals("empty")) {
                 if (queue.isEmpty()){
-                    System.out.println(1);
+                    sb.append(1).append('\n');
                 }
                 else {
-                    System.out.println(0);
+                    sb.append(0).append('\n');
                 }
             } else if (str.equals("front")) {
                 if (queue.isEmpty()){
-                    System.out.println(-1);
+                    sb.append(-1).append('\n');
                 }
                 else {
-                    System.out.println(queue.peek());
+                    sb.append(queue.peek()).append('\n');
                 }
             } else if (str.equals("back")) {
                 if (queue.isEmpty()){
-                    System.out.println(-1);
+                    sb.append(-1).append('\n');
+                    
                 }
                 else {
                     for (int i = 0; i < queue.size(); i++) {
                         if (i == queue.size()-1){
-                            System.out.println(queue.peek());
+                            sb.append(queue.peek()).append('\n');
                         }
                         queue.add(queue.poll());
                     }
                 }
             }
         }
+        System.out.println(sb);
     }
 }
